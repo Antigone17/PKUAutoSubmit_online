fbb264e7db1a04ceeeb391bebb6afe03bcc75dc5
Antigone17 <38316151+Antigone17@users.noreply.github.com>
2021-11-17 09:34:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,14 +27,14 @@ def go(config):
     conf = ConfigParser()
     conf.read(config, encoding='utf8')
 
-    campus, reason = dict(conf['common']).values()
+    campus, reason, d_reason = dict(conf['common']).values()
     destination, track = dict(conf['out']).values()
     habitation, district, street = dict(conf['in']).values()
     capture = conf.getboolean('capture', '是否需要备案历史截图')
     path = conf['capture']['截图保存路径']
     wechat = conf.getboolean('wechat', '是否需要微信通知')
 
-    run(driver_pjs, argconf.ID, argconf.PASSWORD, campus, argconf.MAIL_ADDRESS, argconf.PHONE_NUMBER, reason, destination, track,
+    run(driver_pjs, argconf.ID, argconf.PASSWORD, campus, reason, d_reason, destination, track,
         habitation, district, street, capture, path, wechat, argconf.SENDKEY)
 
 
@@ -43,10 +43,12 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--ID', type=str)
     parser.add_argument('--PASSWORD', type=str)
-    parser.add_argument('--MAIL_ADDRESS', type=str)
-    parser.add_argument('--PHONE_NUMBER', type=str)
     parser.add_argument('--SENDKEY', type=str)
     argconf = parser.parse_args()
+
+    # driver_pjs = webdriver.PhantomJS(
+    #     executable_path=sys_path(browser="phantomjs"),
+    #     service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -56,6 +58,19 @@ if __name__ == '__main__':
             service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
     print('Driver Launched\n')
 
+    lst_conf = sorted([
+        fileName for fileName in os.listdir()
+        if re.match(r'^config[0-9][0-9]*\.ini$', fileName)
+    ],
+        key=lambda x: int(re.findall(r'[0-9]+', x)[0]))
+
+    print(f'读取到{len(lst_conf)+1}份配置文件\n')
+    print('||第1个学生备案||')
     go('config.ini')
+
+    if lst_conf:
+        for num, config in enumerate(lst_conf):
+            print(f'||第{num+2}个学生备案||')
+            go(config)
 
     driver_pjs.quit()
